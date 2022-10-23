09f8bb768852e2adbdaf0a189bb1220285d739ac
Mali Gaurav <maligaurav947@gmail.com>
2022-10-23 23:01:28 +0530

    V0.4

--- src/components/Pages/asst/Header.js
+++ src/components/Pages/asst/Header.js
@@ -1,33 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { AiOutlineStar, AiOutlineClose } from "react-icons/ai";
-import { BsThreeDots } from "react-icons/bs";
+import { AiOutlineStar } from "react-icons/ai";
+import { BsFillGridFill } from "react-icons/bs";
 
 function Header() {
+  const [status, setStauts] = React.useState(false);
   return (
     <>
       <header className="main-header bg-screen">
-        <BsThreeDots className="open" />
-        <AiOutlineClose className="close" />
+        <BsFillGridFill
+          className="open"
+          onClick={() => {
+            setStauts(!status);
+          }}
+        />
         <div className="main-header__logo">
           <p>LibraryL</p>
         </div>
-        <div className="main-header__list">
-          <ul className="main-header__list__navbar">
-            <Link to="/" exact>
-              <li className="main-header__list__navbar__item">Home</li>
-            </Link>
-            <Link to="/List" exact>
-              <li className="main-header__list__navbar__item">List</li>
-            </Link>
-            <Link to="/About" exact>
-              <li className="main-header__list__navbar__item">About</li>
-            </Link>
-            <Link to="/Contact" exact>
-              <li className="main-header__list__navbar__item">Contact</li>
-            </Link>
-          </ul>
-        </div>
+        {status ? (
+          <div className="main-header__list">
+            <ul className="main-header__list__navbar">
+              <Link to="/" exact>
+                <li className="main-header__list__navbar__item">Home</li>
+              </Link>
+              <Link to="/List" exact>
+                <li className="main-header__list__navbar__item">List</li>
+              </Link>
+              <Link to="/About" exact>
+                <li className="main-header__list__navbar__item">About</li>
+              </Link>
+              <Link to="/Contact" exact>
+                <li className="main-header__list__navbar__item">Contact</li>
+              </Link>
+            </ul>
+          </div>
+        ) : null}
         <div className="main-header__icon">
           <a
             href="https://github.com/maligaurav947/library-list-react"
